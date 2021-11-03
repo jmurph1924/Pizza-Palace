20d8380f0c0c0207993523278eea64bb943be499
jmurph1924 <jtmurphree@bellsouth.net>
2021-11-02 21:22:21 -0400

    master

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button } from '../ButtonElements'
 
-import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img} from './InfoElements'
+import { InfoContainer, InfoWrapper, InfoRow, Column1, 
+    Column2, TextWrapper, TopLine, Heading, Subtitle, 
+    BtnWrap, ImgWrap, Img} from './InfoElements'
 
 export const InfoSection = (lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, buttonLabel, img, alt) => {
     return (
