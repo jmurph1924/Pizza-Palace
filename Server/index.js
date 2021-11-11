8bae64e9b5f72238f72a1a1d52f87d298b77613b
jmurph1924 <jtmurphree@bellsouth.net>
2021-11-10 20:08:09 -0500

    master

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,39 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql')
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const mysql = require('mysql');
 
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Cr8dlek33per1!',
+    password: 'password',
     database: 'pizzadatabase',
-})
+});
 
-app.get("/", (req, res) =>{
-    
-    const sqlInsert = "INSERT INTO customer (customer_name, phone_num) VALUES ('John Snow', 6786686666);"
-    db.query(sqlInsert, (err, result) => {
-       res.send("hello Jonathan"); 
-    })
-    
+app.use(cors())
+app.use(express.json())
+app.use (express.urlencoded ({extended: true}));
+
+
+app.post("/api/insert", (req, res) => {
+
+const customer_name = req.body.customer_name;
+const phone_num = req.body.phone_num;
+const c_password = req.body.c_password;
+const ADDRESS = req.body.ADDRESS;
+const CITY = req.body.CITY;
+const STATE = req.body.STATE;
+const COUNTRY = req.body.COUNTRY;
+const ZIPCODE = req.body.ZIPCODE;
+
+    const sqlInsert = "INSERT INTO customer (customer_name, phone_num, c_password) VALUES (?,?,?)";
+    const sqlInsert2 = "INSERT INTO address (ADDRESS, CITY, STATE, COUNTRY, ZIPCODE) VALUES (?,?,?,?,?)";
+    db.query(sqlInsert, [customer_name, phone_num, c_password], (err, result) => {
+        console.log(err)
+    });
+    db.query(sqlInsert2, [ADDRESS, CITY, STATE, COUNTRY, ZIPCODE], (err, result) => {
+        console.log(err)
+    });
 });
 
 app.listen(3001, () => {
